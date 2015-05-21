19ce497b8bfadf760a680bbdac250f2abdc28218
Azz1 <azlichun@gmail.com>
2015-05-20 21:42:23 -0400

    star loc

---
+++ StarLocator/StarLocator.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+
+# Python library for Star Locator and tracking
+
+import math
+
+
+class StarLocator:
+
+    def __init__(self):
+
+
+# Simple example prints accel/mag data once per second:
+if __name__ == '__main__':
+
+    from time import sleep
