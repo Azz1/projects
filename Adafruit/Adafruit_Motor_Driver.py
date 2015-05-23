50e978c6f17a43970e03a31472c96abd9be36bea
Jack Zhu <azlichun@gmail.com>
2015-05-23 00:26:20 -0400

    ip change

--- Adafruit/Adafruit_Motor_Driver.py
+++ Adafruit/Adafruit_Motor_Driver.py
@@ -301,20 +301,20 @@ class StepMotor :
     if dir == 'FORWARD' and self.FWD_pin > 0:
       #print 'FORWARD pin %s check ...' % str(self.FWD_pin)
       if GPIO.input(self.FWD_pin) :
-        time.sleep(0.03)	
+        #time.sleep(0.03)	
         if GPIO.input(self.FWD_pin) :		#check again after 0.1s in case of false positive
-          #print 'FORWARD pin %s raised!' % str(self.FWD_pin)
-          return self.currentstep 
-	  #pass
+          print 'FORWARD pin %s raised!' % str(self.FWD_pin)
+          #return self.currentstep 
+	  pass
 
     elif dir == 'BACKWARD' and self.BKWD_pin > 0:
       #print 'BACKWARD pin %s check ...' % str(self.BKWD_pin)
       if GPIO.input(self.BKWD_pin) :
-        time.sleep(0.03)	
+        #time.sleep(0.03)	
         if GPIO.input(self.BKWD_pin) :		#check again after 0.1s in case of false positive
           #print 'BACKWARD pin %s raised!' % str(self.BKWD_pin)
-          return self.currentstep 
-	  #pass
+          #return self.currentstep 
+	  pass
 
     # next determine what sort of stepping procedure we're up to
     if style == 'SINGLE' :
--- Web/start.sh
+++ Web/start.sh
@@ -1,2 +1,2 @@
 sudo nodejs  ../nodejs/stream-server.js soul &
-sudo python httpserver.py 8080 192.168.111.101
+sudo python httpserver.py 8080 192.168.111.81
