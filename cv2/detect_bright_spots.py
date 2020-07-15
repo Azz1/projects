c9787d894c768f3cde64a76199959c4822e442e2
Azz1 <azlichun@gmail.com>
2020-07-14 23:06:29 -0400

    cv2 test

--- cv2/detect_bright_spots.py
+++ cv2/detect_bright_spots.py
@@ -40,7 +40,8 @@ class CV2Helper :
         # perform a connected component analysis on the thresholded
         # image, then initialize a mask to store only the "large"
         # components
-        labels = measure.label(thresh, connectivity=2, background=0)
+        #labels = measure.label(thresh, connectivity=2, background=0)
+        labels = measure.label(thresh, neighbors=8, background=0)
         mask = np.zeros(thresh.shape, dtype="uint8")
         # loop over the unique components
         for label in np.unique(labels):
